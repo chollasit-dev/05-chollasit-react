c0b9ae1bd649c90cbe3d8d5ccc0074220c98c829
chollasit-dev <118034127+chollasit-dev@users.noreply.github.com>
2025-04-02 13:30:20 +0700

    feat(ui): create simple homepage

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,8 +1,10 @@
 export function Index() {
   return (
-    <section className='mx-auto content-center lg:min-h-[calc(100dvh-4rem-5rem)]'>
+    <section className='mx-auto min-h-[calc(100dvh-4rem-5rem)] content-center'>
       <div className='mx-auto max-w-7xl'>
-        <h1 className='text-center text-3xl'>Index</h1>
+        <h1 className='text-wrap text-center text-5xl font-medium'>
+          Generation Thailand React Assessment
+        </h1>
       </div>
     </section>
   );
